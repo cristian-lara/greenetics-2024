e1d36bbcffa480a823af44e31f0b7919beba118f
Cristian Lara <cristian89lara@gmail.com>
2024-06-21 21:42:47 -0600

    feat: prueba actualizada

--- poc-seguridad/src/App.test.js
+++ poc-seguridad/src/App.test.js
@@ -1,7 +1,31 @@
 import App from "./App";
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 test('renders App component', () => {
     const { container } = render(<App />);
     expect(container).toBeInTheDocument();
   });
+
+
+test('evitar xss sanitizando la entrada', ()=>{
+    // interceptar todos los "alert". Se verifica si se llama a alert
+
+    window.alert = jest.fn();
+    
+    const {getByText,getByTestId, getByLabelText, getByRol,} = render(<App/>)
+
+    // Simular la entrada con un script malicioso
+
+    const input = getByTestId('inputNombre');
+    fireEvent.change(input, {target: {value:"<img src=x onerror=alert('XSS') />"}})
+console.log('input', input.value)
+
+    const enviarBtn = getByText('Enviar');
+    fireEvent.click(enviarBtn);
+    // Verificar que el script no se ejecuta
+    expect(window.alert).not.toHaveBeenCalled;
+
+
+    //VErificar que se visualice el contenido deseado
+
+})
